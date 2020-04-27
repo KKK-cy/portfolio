04475518ed7c02f4c2583c86fbf166cf2e746eb1
Unknown <1298158291@qq.com>
2020-04-27 21:00:05 +0800

    完善了galleryAPP，设置了媒体路径

--- gallery/models.py
+++ gallery/models.py
@@ -4,6 +4,13 @@ from django.db import models
 """ 创建app要实现的功能 """
 
 class Gallery(models.Model):
-    description = models.CharField(max_length=100)
+    # 作品描述
+    description = models.CharField(default="在这里写作品的简介",max_length=100)
+    # 作品图片(可以指定默认图片为default.png，这样创建gallery没有上传图片时会有默认图片;还可以指定图片的上传路径)
+    img = models.ImageField(default='default.png',upload_to='images/')
+    # 作品标题
+    title = models.CharField(default='作品标题',max_length=30)
 
-
+    # admin管理后台就会显示作品的标题
+    def __str__(self):
+        return self.title
